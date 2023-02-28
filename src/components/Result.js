36d5c6ec18b352b1083fbf786273bfbe1966ca81
Sofia13066 <podolskaya.sofya190498@gmail.com>
2023-02-28 03:43:50 +0200

    modified:   src/components/Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,52 +3,49 @@ import { useStateWithCallback } from '../actions/useStateWithCallback';
 
 const Result = (props) => {
 
-    let res;
-    // const [isWin, setIsWin] = useState(0);
-    // const [isLoss, setIsLoss] = useState(0);
+    // let res;
 
 
     const getResult = () => {
         
         if (props.playerWins > props.compWins){
 
-            let res = () => props.setStoryWins(current => current + 1)
-            // res()
-            console.log(props.storyWins);
-            // setIsWin(1);
-            // console.log(isWin);
+            // let res = () => props.setStoryWins(current => current + 1)
+
+            
             return 'You won'
 
         }
         if (props.playerWins < props.compWins){
             
             // let res = () => props.setStoryLosses(props.storyLosses => props.storyLosses + 1)
-            const def = (props) => { return (props.setStoryLosses(current => current + 1)); };
+            // const def = (props) => { return (props.setStoryLosses(current => current + 1)); };
         
-            // setIsLoss(1);
-            // console.log(isLoss);
             return 'You lost'
 
         }
-        return 'DRAW'
+        
+        return 'Draw'
     }
 
-    useEffect(() => {
-        res = getResult();
-        if(res == "You won"){
-        let res = () => props.setStoryWins(current => current + 1)
-        // props.setStoryWins(
-        //     (prevState) => prevState + 1,
-        //     (prevState, currentState) => dispatch(myaction(prevState))
-        //   );
-        }
+    // useEffect(() => {
+    //     res = getResult();
+    //     if(res == "You won"){
+    //     let res = () => props.setStoryWins(current => current + 1)
+    //     // props.setStoryWins(
+    //     //     (prevState) => prevState + 1,
+    //     //     (prevState, currentState) => dispatch(myaction(prevState))
+    //     //   );
+    //     }else if(res == "You lost"){
+    //         let res = () => props.setStoryWins(current => current + 1)
+    //     }
 
-    }, [])
+    // }, [])
     
 
     return (
         <div>
-            <h1>{res} the last game</h1>
+            <h1>{getResult()} in the last game</h1>
             <h2>{props.playerWins}-{props.compWins}</h2><br/>
             <h2>Game statistics</h2>
             <h2>Wins: {props.storyWins} ---- Losses: {props.storyLosses}</h2>
